465a0b702594047e1d803ced476a0337ac55dbd1
isshy <>
2025-12-07 09:54:33 +0900

    シンプル化

--- next.config.js
+++ next.config.js
@@ -1,10 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  distDir: 'build',
   output: 'standalone',
   env: {
     API_ENDPOINT: process.env.API_ENDPOINT,
-  },
-};
+  }
+}
 
 module.exports = nextConfig
