f4c3da799da47e537d5ad83ed2940632a9b285d5
isshy <>
2025-12-07 13:11:44 +0900

    動いたVerをBildとDeployに分割

--- next.config.js
+++ next.config.js
@@ -1,9 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // ビルド結果を .next ではなく ./build に出す
+  distDir: 'build',
+  // standalone 出力（Azure / Node 自前ホスティング向け）
   output: 'standalone',
   env: {
     API_ENDPOINT: process.env.API_ENDPOINT,
-  }
-}
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
